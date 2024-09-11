7af5d69625611793ec28cdb3871e96ca506183de
sergio <sergiosan2802@gmail.com>
2024-09-11 16:36:57 -0500

    feat: usign envs - firebase config

--- src/config/firebase/index.ts
+++ src/config/firebase/index.ts
@@ -1,14 +1,22 @@
 import { initializeApp } from "firebase/app";
 
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINSENDERID,
+  VITE_APPID,
+} = import.meta.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD2wkVre7drPJgNqoavlf91ngr4nUqP3D4",
-  authDomain: "digital-adventure-api.firebaseapp.com",
-  projectId: "digital-adventure-api",
-  storageBucket: "digital-adventure-api.appspot.com",
-  messagingSenderId: "690886475744",
-  appId: "1:690886475744:web:9a16c7d60cae7e53377585",
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINSENDERID,
+  appId: VITE_APPID,
 };
 
-// Initialize Firebase
 const appFirebase = initializeApp(firebaseConfig);
 export default appFirebase;
