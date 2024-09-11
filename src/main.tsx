ef1d6d66db552447466cf256ad1bf166df1ce2f7
sergio <sergiosan2802@gmail.com>
2024-09-11 16:35:44 -0500

    feat: add router lib, sass, implement styles modules, config sass - vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
+import { RouterApp } from "./router";
+
+import "./common/styles/global.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <RouterApp />
+    </BrowserRouter>
   </StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import sass from "sass";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implmentation: sass,
+      },
+    },
+  },
 });
