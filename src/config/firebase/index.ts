fd58d2e715971fc004039029361763a335e277d9
sergio <sergiosan2802@gmail.com>
2024-09-12 01:49:08 -0500

    feat: modify variables names

--- src/config/firebase/index.ts
+++ src/config/firebase/index.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 const {
-  VITE_APIKEY,
+  VITE_API_KEY,
   VITE_AUTHDOMAIN,
   VITE_PROJECTID,
   VITE_STORAGEBUCKET,
@@ -10,7 +10,7 @@ const {
 } = import.meta.env;
 
 const firebaseConfig = {
-  apiKey: VITE_APIKEY,
+  apiKey: VITE_API_KEY,
   authDomain: VITE_AUTHDOMAIN,
   projectId: VITE_PROJECTID,
   storageBucket: VITE_STORAGEBUCKET,
