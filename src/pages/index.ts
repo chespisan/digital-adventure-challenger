759282ecea5f824b79f6dcc86d0eb04333fff11e
sergio <sergiosan2802@gmail.com>
2024-09-13 09:41:29 -0500

    feat: implement upload page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export { HomePage } from "../pages/home";
 export { AuthPage } from "../pages/auth";
 export { OnboardingPage } from "../pages/onboarding";
 export { PhotoManagementPage } from "./photo-management";
+export { UploadPage } from "./upload";
