cf0d3e126fe922cb7bae215515c45547ddf6fe9b
sergio <sergiosan2802@gmail.com>
2024-09-12 21:22:24 -0500

    feat: update homePage - v1

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,29 +1,68 @@
-import { useNavigate } from "react-router-dom";
-import { AuthService } from "../../services/auth";
-import { ButtonComponent } from "../../common/components";
-import { useCookies } from "react-cookie";
+import { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { FaCamera, FaHeart, FaPaperPlane } from "react-icons/fa";
 
-const { logout } = AuthService.getInstance();
+import { MenuBarComponent } from "../../common/components";
+import { PhotoManagementService } from "../../services/photo";
+import { IPhoto } from "../../services/photo/interface";
+
+import "./home.scss";
 
 export const HomePage = () => {
   const navigate = useNavigate();
-  const [_user, __user, removeUserCookie] = useCookies(["user"]);
-  const [_token, __token, removeTokenCookie] = useCookies(["access_token"]);
+  const [gallery, setGallery] = useState<IPhoto[]>();
 
-  const signOut = async () => {
-    try {
-      await logout();
-      removeUserCookie("user");
-      removeTokenCookie("access_token");
-      navigate("/onboarding");
-    } catch (error) {
-      console.log(`Error logout`, error);
+  const goToPage = (path: string) => {
+    navigate(path);
+  };
+
+  const geAllGallery = async () => {
+    const res = await PhotoManagementService.getInstance().getAllGallery();
+    if (res?.isError) {
+      return;
+    }
+    if (res?.galleryPhotos) {
+      setGallery(res.galleryPhotos);
     }
   };
 
+  useEffect(() => {
+    geAllGallery();
+  }, []);
+
   return (
-    <div>
-      <ButtonComponent action={signOut} color="primary" text="Cerrar sesion" />
+    <div className="home">
+      <MenuBarComponent />
+
+      <div className="home__container">
+        {gallery?.map((photo) => (
+          <div key={photo.id} className="home__card">
+            <div className="home__card-img">
+              <img
+                className="home__img"
+                src={photo.path}
+                alt="image card"
+                width="400"
+                height="400"
+                sizes="(max-width: 640px) 400px,
+                        800px"
+              />
+            </div>
+            <div className="home__actions-card">
+              <div>
+                <FaHeart size="16" />
+                {photo.likes}
+              </div>
+              <FaPaperPlane size="16" />
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <button className="button-float" onClick={() => goToPage("/photo")}>
+        <FaCamera className="button-float__icon" />
+      </button>
+      <Outlet />
     </div>
   );
 };
